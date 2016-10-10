492e7a1efd7207a72ff5e5e773b4803d39016d0a
alejandrobrazabarba <alejandrobrazabarba@gmail.com>
2016-10-10 19:07:58 +0200

    Added some comments about how the setTarget works in cw.py and stop.py

--- cw.py
+++ cw.py
@@ -2,5 +2,11 @@
 
 import maestro as m
 s = m.Controller()
-s.setTarget(4,1)
-s.setTarget(5,1)
+#The arguments of setTarget are the channel to which the servo is connected
+#and the pulse width in quarters of microseconds
+#So for a servo with resting position at 1500 us, you need to send 4*1500=6000
+#If you send a 0 it stop sending pulses
+#Because of the way the library works with binary displacements, sending -1 as
+#target is equivalent to sending 16384=2^14
+s.setTarget(4,7500)
+s.setTarget(5,4500)
--- stop.py
+++ stop.py
@@ -2,5 +2,9 @@
 
 import maestro as m
 s = m.Controller()
+
+#This is equivalent to send a 6000 as target
+#but this method is more efficient as it makes the micro maestro stops
+#sending pulses to the servo
 s.setTarget(4,0)
 s.setTarget(5,0)
